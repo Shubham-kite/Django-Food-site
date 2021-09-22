cc231f27b9a6f698e98f6b3eb6e9bf823ec15b76
shubham-kite <shubhamkites099@gmail.com>
2021-09-22 11:09:40 +0530

    Don't know exact one

--- HappyMeals/urls.py
+++ HappyMeals/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from HappyMeals import views
 urlpatterns = [
     path('',views.index,name="index"),
+    path('index',views.index,name="index"),
     path('sign_in', views.sign_in,name="sign in"),
     path('sign_up',views.sign_up,name="Sign up"),
     path('order', views.order,name="order")
--- HappyMeals/views.py
+++ HappyMeals/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,HttpResponse,redirect
 from HappyMeals.models import customer
 from datetime import datetime
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate,login
+
+
 def index(request):
     return render(request,'index.html')
 
@@ -10,12 +12,12 @@ def sign_in (request):
     if request.method=="POST":
         email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(email=email, password=password)
-        print(user)
-        if user is not None:
-            login(request, user)
-            return redirect('./order')
-        else:    
+        try:
+            data =customer.objects.get(email=email,password=password)
+            if ((data.email == email) & (data.password == password)):
+                # login(request, user)
+                return redirect('./order')
+        except:   
             return redirect('./sign_in')
     return render(request,'signin.html')            
    
@@ -26,10 +28,15 @@ def sign_up(request):
         password = request.POST.get('password')
         phone = request.POST.get('phone')
         address = request.POST.get('address')
-        info =customer(date =datetime.today(),name=name,email=email,password=password,phone=phone,address=address)
-        info.save()   
+        info = customer(date =datetime.today(),name=name,email=email,password=password,phone=phone,address=address)
+        info.save()
+        return redirect('./sign_in')   
     return render(request,'signup.html')
+
 def order(request):
-    return render(request,'order.html')    
+    return render(request,'order.html') 
+
+def logout(request):
+    return render(request,'signin.html')
        
 
